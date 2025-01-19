caa1c04e6ddb1bc5940eb6c0ba15edf0517020b9
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-20 02:26:30 +0700

    feat(main): tied conf, db, server and start server already

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main(){
-	
+import (
+	"github.com/Kittisak2001/isekai-shop-api/config"
+	"github.com/Kittisak2001/isekai-shop-api/databases"
+	"github.com/Kittisak2001/isekai-shop-api/server"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
 }
