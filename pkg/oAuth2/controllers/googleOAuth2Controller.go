60f87892318b8e7260c232588b3caf2f0802c8ee
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-24 00:15:28 +0700

    refactor: config oauth

--- pkg/oAuth2/controllers/googleOAuth2Controller.go
+++ pkg/oAuth2/controllers/googleOAuth2Controller.go
@@ -3,9 +3,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
-	"sync"
 	"time"
-
 	"github.com/Kittisak2001/isekai-shop-api/config"
 	"github.com/Kittisak2001/isekai-shop-api/pkg/custom"
 	"github.com/Kittisak2001/isekai-shop-api/pkg/oAuth2/exception"
@@ -13,7 +11,6 @@ import (
 	"github.com/Kittisak2001/isekai-shop-api/pkg/oAuth2/services"
 	"github.com/labstack/echo/v4"
 	"golang.org/x/exp/rand"
-	"golang.org/x/oauth2"
 )
 
 type (
@@ -25,113 +22,87 @@ type (
 )
 
 var (
-	playerGoogleOAuth2 *oauth2.Config
-	adminGoogleOAuth2  *oauth2.Config
-	once               sync.Once
-
-	accessTokenCookieName  = "act"
-	refreshTokenCookieName = "rft"
-	stateCookieName        = "state"
-
-	letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 )
 
 func NewGoogleOAuth2Controller(oAuth2Service services.OAuth2Service, oAuth2Conf *config.OAuth2Cfg, logger echo.Logger) *googleOAuth2Controller {
-	once.Do(func() {
-		setGoogleOAuth2Config(oAuth2Conf)
-	})
-
 	return &googleOAuth2Controller{oAuth2Service, oAuth2Conf, logger}
-}
-
-func setGoogleOAuth2Config(oauth2Conf *config.OAuth2Cfg) {
-	playerGoogleOAuth2 = &oauth2.Config{
-		ClientID:     oauth2Conf.ClientID,
-		ClientSecret: oauth2Conf.ClientSecret,
-		RedirectURL:  oauth2Conf.PlayerRedirectUrl,
-		Scopes:       oauth2Conf.Scopes,
-		Endpoint: oauth2.Endpoint{
-			AuthURL:       oauth2Conf.Endpoints.AuthUrl,
-			TokenURL:      oauth2Conf.Endpoints.TokenUrl,
-			DeviceAuthURL: oauth2Conf.Endpoints.DeviceAuthUrl,
-			AuthStyle:     oauth2.AuthStyleInParams,
-		},
-	}
-
-	adminGoogleOAuth2 = &oauth2.Config{
-		ClientID:     oauth2Conf.ClientID,
-		ClientSecret: oauth2Conf.ClientSecret,
-		RedirectURL:  oauth2Conf.AdminRedirectUrl,
-		Scopes:       oauth2Conf.Scopes,
-		Endpoint: oauth2.Endpoint{
-			AuthURL:       oauth2Conf.Endpoints.AuthUrl,
-			TokenURL:      oauth2Conf.Endpoints.TokenUrl,
-			DeviceAuthURL: oauth2Conf.Endpoints.DeviceAuthUrl,
-			AuthStyle:     oauth2.AuthStyleInParams,
-		},
-	}
 }
 
 func (c *googleOAuth2Controller) PlayerLogin(pctx echo.Context) error {
 	rand.Seed(uint64(time.Now().UnixNano()))
 	state := c.randomState()
-	c.setCookie(pctx, stateCookieName, state)
-	return pctx.Redirect(http.StatusFound, playerGoogleOAuth2.AuthCodeURL(state))
+	c.setCookie(pctx, config.StateCookieName, state)
+	return pctx.Redirect(http.StatusFound, config.PlayerGoogleOAuth2.AuthCodeURL(state))
 }
 
 func (c *googleOAuth2Controller) AdminLogin(pctx echo.Context) error {
 	rand.Seed(uint64(time.Now().UnixNano()))
 	state := c.randomState()
-	c.setCookie(pctx, stateCookieName, state)
-	return pctx.Redirect(http.StatusFound, adminGoogleOAuth2.AuthCodeURL(state))
+	c.setCookie(pctx, config.StateCookieName, state)
+	return pctx.Redirect(http.StatusFound, config.AdminGoogleOAuth2.AuthCodeURL(state))
 }
 
 func (c *googleOAuth2Controller) PlayerLoginCallback(pctx echo.Context) error {
+	ctx := pctx.Request().Context()
 	if err := c.callbackValidating(pctx); err != nil {
 		c.logger.Errorf("Error validating callback: %s", err.Error())
 		return custom.Error(pctx, http.StatusUnauthorized, &exception.FailCallback{Role: "player"})
 	}
 
-	token, userInfo, err := c.oAuth2Service.Callback(pctx.Request().Context(), playerGoogleOAuth2, pctx.QueryParam("code"), c.oAuth2Conf.UserInfoUrl)
+	token, err := c.oAuth2Service.Callback(ctx, config.PlayerGoogleOAuth2, pctx.QueryParam("code"))
 	if err != nil {
 		return custom.Error(pctx, http.StatusUnauthorized, &exception.FailCallback{Role: "player"})
 	}
-	if err := c.oAuth2Service.PlayerAccountCreating(userInfo); err != nil {
-		return custom.Error(pctx, http.StatusUnauthorized, &exception.PlayerCreating{PlayerID: userInfo.Sub})
+	userInfo, err := c.oAuth2Service.GetUserInfo(ctx, config.PlayerGoogleOAuth2, token, c.oAuth2Conf.UserInfoUrl)
+	if err != nil {
+		return custom.Error(pctx, http.StatusUnauthorized, &exception.FailCallback{Role: "player"})
 	}
-	c.setSameSiteCookie(pctx, accessTokenCookieName, token.AccessToken)
-	c.setSameSiteCookie(pctx, refreshTokenCookieName, token.RefreshToken)
+
+	if err := c.oAuth2Service.PlayerAccountCreating(userInfo); err != nil {
+		return custom.Error(pctx, http.StatusUnauthorized, &exception.PlayerCreating{PlayerID: userInfo.ID})
+	}
+	c.setSameSiteCookie(pctx, config.AccessTokenCookieName, token.AccessToken)
+	c.setSameSiteCookie(pctx, config.RefreshTokenCookieName, token.RefreshToken)
 	return pctx.JSON(http.StatusOK, &model.LoginResponse{Message: "Login success"})
 }
 
 func (c *googleOAuth2Controller) AdminLoginCallback(pctx echo.Context) error {
-	token, userInfo, err := c.oAuth2Service.Callback(pctx.Request().Context(), adminGoogleOAuth2, pctx.QueryParam("code"), c.oAuth2Conf.UserInfoUrl)
+	ctx := pctx.Request().Context()
+	if err := c.callbackValidating(pctx); err != nil {
+		c.logger.Errorf("Error validating callback: %s", err.Error())
+		return custom.Error(pctx, http.StatusUnauthorized, &exception.FailCallback{Role: "admin"})
+	}
+	token, err := c.oAuth2Service.Callback(ctx, config.PlayerGoogleOAuth2, pctx.QueryParam("code"))
 	if err != nil {
-		return custom.Error(pctx, http.StatusUnauthorized, &exception.FailCallback{Role: "player"})
+		return custom.Error(pctx, http.StatusUnauthorized, &exception.FailCallback{Role: "admin"})
 	}
-	if err := c.oAuth2Service.PlayerAccountCreating(userInfo); err != nil {
-		return custom.Error(pctx, http.StatusUnauthorized, &exception.PlayerCreating{PlayerID: userInfo.Sub})
+	userInfo, err := c.oAuth2Service.GetUserInfo(ctx, config.PlayerGoogleOAuth2, token, c.oAuth2Conf.UserInfoUrl)
+	if err != nil {
+		return custom.Error(pctx, http.StatusUnauthorized, &exception.FailCallback{Role: "admin"})
 	}
-	c.setSameSiteCookie(pctx, accessTokenCookieName, token.AccessToken)
-	c.setSameSiteCookie(pctx, refreshTokenCookieName, token.RefreshToken)
+	if err := c.oAuth2Service.AdminAccountCreating(userInfo); err != nil {
+		return custom.Error(pctx, http.StatusUnauthorized, &exception.PlayerCreating{PlayerID: userInfo.ID})
+	}
+	c.setSameSiteCookie(pctx, config.AccessTokenCookieName, token.AccessToken)
+	c.setSameSiteCookie(pctx, config.RefreshTokenCookieName, token.RefreshToken)
 	return pctx.JSON(http.StatusOK, &model.LoginResponse{Message: "Login success"})
 }
 
 func (c *googleOAuth2Controller) callbackValidating(pctx echo.Context) error {
 	state := pctx.QueryParam("state")
-	stateFromCookie, err := pctx.Cookie(stateCookieName)
+	stateFromCookie, err := pctx.Cookie(config.StateCookieName)
 	if err != nil {
 		return &exception.ProcessCookie{}
 	}
 	if state != stateFromCookie.Value {
 		return &exception.InvalidState{}
 	}
-	c.removeCookie(pctx, stateCookieName)
+	c.removeCookie(pctx, config.StateCookieName)
 	return nil
 }
 
 func (c *googleOAuth2Controller) Logout(pctx echo.Context) error {
-	accessToken, err := pctx.Cookie(accessTokenCookieName)
+	accessToken, err := pctx.Cookie(config.AccessTokenCookieName)
 	if err != nil && err.Error() != "http: named cookie not present" {
 		c.logger.Errorf("Error reading access token: %s", err.Error())
 		return &exception.ProcessCookie{}
@@ -142,8 +113,8 @@ func (c *googleOAuth2Controller) Logout(pctx echo.Context) error {
 			return &exception.ProcessCookie{}
 		}
 	}
-	c.removeSameSiteCookie(pctx, accessTokenCookieName)
-	c.removeSameSiteCookie(pctx, refreshTokenCookieName)
+	c.removeSameSiteCookie(pctx, config.AccessTokenCookieName)
+	c.removeSameSiteCookie(pctx, config.RefreshTokenCookieName)
 	return pctx.JSON(http.StatusOK, &model.LogoutResponse{Message: "Logout successful"})
 }
 
@@ -207,7 +178,7 @@ func (c *googleOAuth2Controller) removeSameSiteCookie(pctx echo.Context, name st
 func (c *googleOAuth2Controller) randomState() string {
 	b := make([]byte, 16)
 	for i := range b {
-		b[i] = letters[rand.Intn(len(letters))]
+		b[i] = config.Letters[rand.Intn(len(config.Letters))]
 	}
 	return string(b)
 }
