4cc03325f6f50f2dadf629b2df23df9b5eb0f1a5
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-24 10:34:47 +0700

    feat(playCoin): init player coin domain

--- entities/playerCoin.go
+++ entities/playerCoin.go
@@ -1,6 +1,9 @@
 package entities
 
-import "time"
+import (
+	"time"
+	"github.com/Kittisak2001/isekai-shop-api/pkg/playerCoin/model"
+)
 
 type (
 	PlayerCoin struct {
@@ -8,5 +11,22 @@ type (
 		PlayerID  string    `gorm:"type:varchar(64);not null;"`
 		Amount    int64     `gorm:"not null;"`
 		CreatedAt time.Time `gorm:"not null;autoCreateTime;"`
+		Coin      int64     `json:"coin"`
 	}
 )
+
+func (e *PlayerCoin) ToPlayerCoinModel() *model.PlayerCoin {
+	return &model.PlayerCoin{
+		ID:        e.ID,
+		PlayerID:  e.PlayerID,
+		Amount:    e.Amount,
+		CreatedAt: e.CreatedAt,
+	}
+}
+
+func (e *PlayerCoin) ToPlayerCoinShowingModel() *model.PlayerCoinShowing {
+	return &model.PlayerCoinShowing{
+		PlayerID: e.PlayerID,
+		Coin: e.Coin,
+	}
+}
