d8319ec34dd688b6fe78c026b2f326761442b19a
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-26 05:53:54 +0700

    feat(model): inject playerItemsFinding

--- pkg/inventory/exceptions/playerItemFinding.go
+++ pkg/inventory/exceptions/playerItemFinding.go
@@ -1,1 +1,13 @@
 package exceptions
+
+import "fmt"
+
+type (
+	PlayerItemsFinding struct {
+		PlayerID string
+	}
+)
+
+func (e *PlayerItemsFinding) Error() string {
+	return fmt.Sprintf("finding player items for PlayerID: %s failed", e.PlayerID)
+}
