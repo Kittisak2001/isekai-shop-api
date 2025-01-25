8304a7cb2bf0627752469382d34f576aba853fb9
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-26 05:46:03 +0700

    feat(controller): inject service in controller of inventory

--- pkg/inventory/controllers/inventoryController.go
+++ pkg/inventory/controllers/inventoryController.go
@@ -1,7 +1,11 @@
 package controllers
 
-type inventoryController struct{}
+import "github.com/Kittisak2001/isekai-shop-api/pkg/inventory/services"
 
-func NewInventoryController() *inventoryController{
-	return &inventoryController{}
+type inventoryController struct {
+	inventoryService services.InventoryService
+}
+
+func NewInventoryController(inventoryService services.InventoryService) *inventoryController {
+	return &inventoryController{inventoryService:inventoryService}
 }
