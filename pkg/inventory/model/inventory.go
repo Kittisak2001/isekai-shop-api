d88df1ecd691301185d09b71b75ccc0916b879be
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-26 05:48:27 +0700

    feat(model): inject

--- pkg/inventory/model/inventory.go
+++ pkg/inventory/model/inventory.go
@@ -1,1 +1,15 @@
 package model
+
+import "github.com/Kittisak2001/isekai-shop-api/pkg/itemShop/model"
+
+type (
+	Inventory struct {
+		Item     model.Item `json:"item"`
+		Quantity uint       `json:"quantity"`
+	}
+
+	ItemQuantityCounting struct{
+		ItemID uint64
+		Quantity uint
+	}
+)
