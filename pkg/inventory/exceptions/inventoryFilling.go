f39996de5e3a4bf0c8ec99a4d25d052c1d82feb0
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-01-26 05:53:35 +0700

    feat(model): inject inventoryFilling

--- pkg/inventory/exceptions/inventoryFilling.go
+++ pkg/inventory/exceptions/inventoryFilling.go
@@ -1,1 +1,14 @@
 package exceptions
+
+import "fmt"
+
+type (
+	InventoryFilling struct {
+		PlayerID string
+		ItemID   uint64
+	}
+)
+
+func (e *InventoryFilling) Error() string {
+	return fmt.Sprintf("inventory filling for PlayerID: %s and ItemID: %d failed", e.PlayerID, e.ItemID)
+}
--- pkg/inventory/exceptions/playerItemRemoving.go
+++ pkg/inventory/exceptions/playerItemRemoving.go
@@ -1,1 +1,13 @@
 package exceptions
+
+import "fmt"
+
+type (
+	PlayerItemRemoving struct {
+		ItemID uint64
+	}
+)
+
+func (e *PlayerItemRemoving) Error() string {
+	return fmt.Sprintf("removing item ItemID: %d failed", e.ItemID)
+}
